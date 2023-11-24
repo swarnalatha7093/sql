dd93b99c6bd2eaf5e439774a1f619fd22e9fcb6c
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-11-24 15:09:28 +0530

    Add files via upload

---
+++ zomato.sql
@@ -0,0 +1,11 @@
+create table zomato.`user`(
+id int,
+name varchar(45),
+email varchar(45),
+street varchar(45),
+location varchar(45),
+city varchar(45),
+state varchar(20),
+country varchar(20),
+pincode int
+);
