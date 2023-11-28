f85dda112df2e235853e7a175777e804c7b969ec
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-11-28 13:43:46 +0530

    user2.sql

--- user2.sql
+++ user2.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `zomato`.`user2` (
-  `id` INT NOT NULL,
-  `user3name` VARCHAR(45) NULL,
-  `user3col` VARCHAR(45) NULL,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+create table  zomato.`user2`(
+id int not null ,
+name varchar (45) null,
+unique index id_unique(`id`asc)
+);
