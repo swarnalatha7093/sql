15c9a51d05ebe7e7e951cb7556227cbfe1ee8dbc
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-12-05 01:46:34 +0530

    alter methods4-12-23

---
+++ alter methods.sql
@@ -0,0 +1,28 @@
+create table zomato.`person`(
+id int primary key auto_increment,
+name varchar(50) not null,
+email varchar(50) null,
+phone int check(length(phone)=10)
+);
+create table zomato.personaddress(
+id int primary key auto_increment,
+street varchar(50) not null,
+location varchar(50) not null,
+city varchar(50) not null,
+state varchar(20) not null,
+country varchar(10) default ('India'),
+personid int not null,
+constraint foreign key personaddress_person_personid(personid) references zomato.person(id)
+);
+select *from zomato.person;
+select *from zomato.personaddress;
+alter table zomato.person add column gender varchar(10);
+alter table zomato.person modify gender varchar(6);
+alter table zomato.person drop column gender;
+alter table zomato.person modify name varchar(50) not null;
+alter table zomato.person modify name varchar(50) null;
+alter table zomato.person add constraint unique_name unique(name); 
+alter table zomato.person drop index unique_name;
+alter table zomato.person add constraint check_name_3 check(length(name)>=3);
+alter table zomato.person drop check chk_name_3;
+
